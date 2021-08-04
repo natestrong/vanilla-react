980e7fcf43bafd0989a578c3df515741a118c86e
nstrong <nathanstrong.fx@gmail.com>
2021-08-04 16:56:02 -0700

    init

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Pet } from "./Pet";
 
-const App = () =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Oliver",
-      animal: "Cat",
-      favoriteThing: "Biting people.",
-    }),
-    React.createElement(Pet, {
-      name: "Maxwell",
-      animal: "Cat",
-      favoriteThing: "Back Scratches.",
-    }),
-    React.createElement(Pet, {
-      name: "Oliver",
-      animal: "Cat",
-      favoriteThing: "Cuddling.",
-    }),
-  ]);
+const App = () => (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Oliver" animal="Cat" favoriteThing="Eating snacks." />
+    <Pet name="Maxwell" animal="Cat" favoriteThing="Back scratches." />
+    <Pet name="Sophie" animal="Cat" favoriteThing="Cuddling." />
+  </div>
+);
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -2,8 +2,8 @@ export const Pet = ({ name, animal, favoriteThing }) => {
   return (
     <div>
       <h2>{name}</h2>
-      <h2>{animal}</h2>
-      <h2>{favoriteThing}</h2>
+      <h3>{animal}</h3>
+      <h3>{favoriteThing}</h3>
     </div>
   );
 };

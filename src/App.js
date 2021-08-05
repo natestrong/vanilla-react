f0f1d3ed8c68333b8187432ddf1a4ef309fb1f98
nstrong <nathanstrong.fx@gmail.com>
2021-08-04 17:30:57 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import ReactDOM from "react-dom";
-import { Pet } from "./Pet";
+import { SearchParams } from "./SearchParams";
 
 const App = () => (
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Oliver" animal="Cat" favoriteThing="Eating snacks." />
-    <Pet name="Maxwell" animal="Cat" favoriteThing="Back scratches." />
-    <Pet name="Sophie" animal="Cat" favoriteThing="Cuddling." />
+    <SearchParams />
   </div>
 );
 
